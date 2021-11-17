2d6dd4749313a6f33c721c9f40c804bf60f91350
kdomn <keith.domnick@hotmail.co.uk>
2021-11-17 11:29:00 +0000

    animal component

---
+++ Mutant_Zoo/src/main/java/com/kob/components/Animal.java
@@ -0,0 +1,71 @@
+package com.kob.components;
+
+public class Customer {
+    private int customerId;
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String name, int age, String phoneNumber, String email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
