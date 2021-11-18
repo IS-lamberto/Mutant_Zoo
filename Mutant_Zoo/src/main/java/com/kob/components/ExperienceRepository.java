982e2dcd1bda941784d79eb4d2d4f463e9d1238d
Owen Lambert <owen.lambert@informed.com>
2021-11-18 12:44:11 +0000

    Added Experience DAO, Repos and edit AnimalDAO

---
+++ Mutant_Zoo/src/main/java/com/kob/components/ExperienceRepository.java
@@ -0,0 +1,7 @@
+package com.kob.components;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExperienceRepository extends CrudRepository<Experience, Integer> {}
